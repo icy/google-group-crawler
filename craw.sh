71d4c8671b810497f617edc62be2cd6c38d262df
Ky-Anh Huynh <kyanh@theslinux.org>
2013-09-23 08:54:11 +0700

    .md format likes backticks

--- craw.sh
+++ craw.sh
@@ -108,7 +108,7 @@ your group's archive. Your data doesn't belong to you! Google also doesn't
 have any good support for their free stuff. Use Google at your own risk.
 
 I'm your saver! You are listening to me because you've kicked me with
-'-h' or '--help' option. If I am kicked by '-sh' or '--bash' option,
+`-h` or `--help` option. If I am kicked by `-sh` or `--bash` option,
 I will give a you bash script to download all *mbox* files of your group.
 For example,
 
@@ -120,27 +120,27 @@ For example,
 In my example, the first run is to download all basic information, included
 list of topics, list of emails sent to the group. This run will print
 very verbose information, you may not be interested in them. Actually,
-in the very last steps, the game comes: All 'wget' commands are printed
+in the very last steps, the game comes: All `wget` commands are printed
 to your standard output. The second run (which is very fast) will capture
-all commands and save them into the file 'wget.sh', that can be executed
-via 'bash wget.sh'.
+all commands and save them into the file `wget.sh`, that can be executed
+via `bash wget.sh`.
 
 You are almost ready to kick me. I just want to say that I am not alone.
-I can't live without some girls; they are: lynx, wget, awk, and bash.
+I can't live without some girls; they are: `lynx`, `wget`, `awk`, and `bash`.
 Buy me some, otherwise I quit!
 
 ### My whispers...
 
-All email addresses (foo@bar.com) are hidden from mbox file. This is
+All email addresses (foo@bar.com) are hidden in *mbox* files. This is
 because mbox file is public, hence the addresses must be hidden from
 the spammers. This is very sad news. Fortunately, you can always write
 a wrapper, to replace all hidden addresses with the real ones.
 
-I write all data to an output directory specified by your '_D_OUTPUT'
+I write all data to an output directory specified by your `_D_OUTPUT`
 environment. If you don't mind, it's the same as your group name.
 
 By default, I will skip any download if the previous output does exist.
-To force me (really!?) to download all stuff, use '_FORCE' environment.
+To force me (really!?) to download all stuff, use `_FORCE` environment.
 For example
 
     _FORCE="please_do_it" ./craw.sh -sh
@@ -151,9 +151,10 @@ depends on your network (aka your money).
 
 ### More whispers?
 
-Okay, I was born after Anh tried many crazy stuff: nodejs, phantomjs,
-Watir, Google Group features. Anh finally found that the 'hash bang'
-(#!) could give all tricks. No explainatnion here. L00LE it for yourself.
+Okay, I was born after Anh tried many crazy stuff: `nodejs`, `phantomjs`,
+`Wati`r, Google Group features (Luckily, notthing works). Anh finally found
+that the *hash bang* (`#!`) could give all tricks. No explainatnion here.
+`L00LE` it for yourself.
 
 New web technology just makes life harder, doesn't it?
 EOF
