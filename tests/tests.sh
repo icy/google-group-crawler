444df529da71ed03f3d3b25257884cecf70a54d4
Ky-Anh Huynh <kyanh@viettug.org>
2018-08-15 12:58:53 +0700

    tests: Improve tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,27 +2,31 @@
 
 _test_public_1() {
   export _GROUP="${_GROUP:-google-group-crawler-public}"
+  export _D_OUTPUT="${_D_OUTPUT:-./${_ORG:+${_ORG}-}${_GROUP}/}"
+  export _F_OUTPUT="${_F_OUTPUT:-./${_ORG:+${_ORG}-}${_GROUP}.sh}"
 
   echo >&2 ""
   echo >&2 ":: --> Testing Public Group $_GROUP (ORG: ${_ORG:-<empty>}) <--"
   echo >&2 ":: --> _WGET_OPTIONS: ${_WGET_OPTIONS:-<empty>}"
   echo >&2 ""
-  echo >&2 ":: Removing $PWD/$_GROUP"
-  rm -rf "$PWD/$_GROUP/"
-  crawler.sh -sh > "$_GROUP.sh" || return 1
-  bash -n "$_GROUP.sh" || return 1
-  bash -x "$_GROUP.sh" || return 1
+  echo >&2 ":: Removing $PWD/$_D_OUTPUT"
+  rm -rf "$PWD/$_D_OUTPUT/"
+  echo >&2 ":: Generating $_F_OUTPUT..."
+  crawler.sh -sh > "$_F_OUTPUT" || return 1
+  bash -n "$_F_OUTPUT" || return 1
+  echo >&2 ":: Executing $_F_OUTPUT..."
+  bash -x "$_F_OUTPUT" || return 1
   crawler.sh -rss || return 1
 
-  grep -Ri "Message-Id:" "$_GROUP/mbox/" \
+  grep -Ri "Message-Id:" "$_D_OUTPUT/mbox/" \
   || {
-    echo >&2 ":: Unable to find any mail messages from $_GROUP/mbox/"
+    echo >&2 ":: Unable to find any mail messages from $_D_OUTPUT/mbox/"
     return 1
   }
 
-  grep -Ri "CICD passed" "$_GROUP/mbox/" \
+  grep -Ri "CICD passed" "$_D_OUTPUT/mbox/" \
   || {
-    echo >&2 ":: Unable to find string 'CICD passed' from $_GROUP/mbox/"
+    echo >&2 ":: Unable to find string 'CICD passed' from $_D_OUTPUT/mbox/"
     return 1
   }
 }
@@ -35,7 +39,7 @@ _test_public_1_with_cat() {
   )
 }
 
-_test_public_2() {
+_test_public_2_with_cookie() {
   (
     export _ORG="viettug.org"
     export _GROUP="google-group-crawler-public2"
@@ -59,5 +63,5 @@ export PATH="$PATH:$(pwd -P)/../"
 
 _test_public_1 || exit 1
 _test_public_1_with_cat || exit 1
-#_test_public_2 || exit 2
+#_test_public_2_with_cookie || exit 2
 # _test_private_1 || exit 3
